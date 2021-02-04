8b040e0b547711aa34889ae1278e6efa9e29c3ec
{bobsini601} <{psh7130@naver.com}>
2021-02-05 04:19:32 +0900

    add backend files

---
+++ hongiknow/hongiknow/src/main/java/web/hongiknow/HongiknowApplication.java
@@ -0,0 +1,13 @@
+package web.hongiknow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongiknowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongiknowApplication.class, args);
+	}
+
+}
---
+++ hongiknow/hongiknow/src/test/java/web/hongiknow/HongiknowApplicationTests.java
@@ -0,0 +1,13 @@
+package web.hongiknow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongiknowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
